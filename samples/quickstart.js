41216f2a3f75d8ec9c18074e0eef4df1c3122b05
Sofia Leon <sofialeon@google.com>
2020-12-03 19:57:35 -0800

    run lint

--- samples/quickstart.js
+++ samples/quickstart.js
@@ -32,7 +32,9 @@ async function main(projectId) {
 
   //TODO(library generator): write the actual function you will be testing
   async function listDomains() {
-    const domains = await client.listDomains({parent: `projects/${projectId}/locations/global`});
+    const domains = await client.listDomains({
+      parent: `projects/${projectId}/locations/global`,
+    });
     console.info(domains);
   }
   listDomains();
